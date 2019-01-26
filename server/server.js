12a973d6e04870f701ce90058b523e0bf81e80bd
Roee-R <roi890@gmail.com>
2019-01-26 16:17:53 +0200

    Update for heroku

--- server/server.js
+++ server/server.js
@@ -8,10 +8,19 @@ const port = process.env.PORT || 3000 ;
 
 var app = express();
 var server = http.createServer(app) // app.listen call this funtion to. //for socket.io use
+var io = socketIo(server) //The server we want to use in socketIo
+// io is the web socket server - the io is the way we will comunicate bitween a server and a client
 
 app.use(express.static(publicPath)); // add the public directory
 
+io.on('connection',(socket)=>{
+    console.log('New connection is made')
 
-app.listen(port, ()=>{ // we change app to server for socket.io use
+    socket.on('disconnect',()=>{
+        console.log('client disconnected from server')
+    })
+}) // the socket event is fired when we get new connection
+
+server.listen(port, ()=>{ // we change app to server for socket.io use
     console.log(`Server is running on port ${port}`);
 })
